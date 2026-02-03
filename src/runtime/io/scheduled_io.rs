29b24fd4e0bd5416f1b68ac0f64a24db3fea331a
Luis Teodoro <luislopesteo@hotmail.com>
2026-02-03 16:26:20 -0300

    Initialized IO representation

---
+++ src/runtime/io/scheduled_io.rs
@@ -0,0 +1,47 @@
+// Respresenta uma fonte de io que foi adicionada ao driver de io
+use std::collections::HashMap;
+use std::pin::Pin;
+use std::rc::Rc;
+use std::task::{Context, Poll, Waker};
+
+use mio::Token;
+
+pub struct ScheduledIo {
+    waker: Option<Waker>,
+    is_ready: bool,
+}
+
+impl ScheduledIo {
+    pub fn default() -> Self {
+        ScheduledIo {
+            waker: None,
+            is_ready: false,
+        }
+    }
+
+    // Handle for waking this future
+    pub fn wake(&self) {
+        self.waker
+            .as_ref()
+            .expect("Waker not provided")
+            .wake_by_ref();
+    }
+
+    pub fn token(&self) -> Token {
+        let ptr = self as *const ScheduledIo;
+        Token(ptr.addr())
+    }
+}
+
+impl Future for ScheduledIo {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let io = unsafe { self.get_unchecked_mut() };
+        if io.is_ready {
+            return Poll::Ready(());
+        }
+        io.waker = Some(cx.waker().clone());
+        Poll::Pending
+    }
+}
