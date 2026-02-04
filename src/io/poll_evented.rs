b0133202f81bea035cdd3e14deb46622176e4358
Luis Teodoro <luislopesteo@hotmail.com>
2026-02-04 16:50:53 -0300

    feat: registration receiving executor handle

--- src/io/poll_evented.rs
+++ src/io/poll_evented.rs
@@ -1,4 +1,4 @@
-use crate::runtime::io::Registration;
+use crate::runtime::{context, io::Registration};
 use mio::event::Source;
 
 pub struct PollEvented<E: Source> {
@@ -7,8 +7,8 @@ pub struct PollEvented<E: Source> {
 }
 
 impl<E: Source> PollEvented<E> {
-    pub fn new(source: E) -> Self {
-        let registration = Registration::register(&source);
+    pub fn new(mut source: E) -> Self {
+        let registration = Registration::register(&mut source, context::get_executor_handle());
         PollEvented {
             io: source,
             registration: registration,
