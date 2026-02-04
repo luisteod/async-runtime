b2ff58cbcb2a0c34626e176c8f9fee8d80051ce1
Luis Teodoro <luislopesteo@hotmail.com>
2026-02-04 16:49:00 -0300

    feat: first registration impl

--- src/runtime/io/registration.rs
+++ src/runtime/io/registration.rs
@@ -1,10 +1,15 @@
+use crate::runtime::{executor::ExecutorHandle, io::scheduled_io::ScheduledIo};
 use mio::event::Source;
+use std::rc::Rc;
 
-pub struct Registration {}
+pub struct Registration {
+    io: Rc<ScheduledIo>,
+}
 
 impl Registration {
-    pub fn register(source: &impl Source) -> Self {
+    pub fn register(source: &mut impl Source, exec_handle: Rc<ExecutorHandle>) -> Self {
         // Get the IoDriver via global context and register
-        Registration {}
+        let io = exec_handle.io().add_source(source);
+        Registration { io }
     }
 }
