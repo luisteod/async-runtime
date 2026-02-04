b535c535345c0213333f76b8eb8e9d9346513a5b
Luis Teodoro <luislopesteo@hotmail.com>
2026-02-04 14:05:48 -0300

    fix: decouple IoDriver in driver and handle

--- src/runtime/io/driver.rs
+++ src/runtime/io/driver.rs
@@ -7,40 +7,38 @@
 // pub fn turn
 
 use super::scheduled_io::ScheduledIo;
-use mio::{Events, Poll, Token, event::Source, Interest};
+use mio::{Events, Interest, Poll, Registry, Token, event::Source};
 use std::collections::HashMap;
 
 const MAX_EVENTS_CAPACITY: usize = 1024;
 const DEFAULT_IO_INTERESTS: Interest = Interest::READABLE.add(Interest::WRITABLE);
 
-pub struct Driver {
+pub struct IoDriver {
     poll: Poll,
+}
+
+pub struct IoHandle {
+    registry: Registry,
     scheduled_io_map: HashMap<Token, ScheduledIo>,
 }
 
-impl Driver {
-    pub fn new() -> Self {
+impl IoDriver {
+    pub fn new() -> (IoDriver, IoHandle) {
         let poll = Poll::new().expect("Error creating Poll instance");
+
+        let registry = poll.registry().try_clone().expect("Error cloning registry");
         let scheduled_io_map = HashMap::new();
 
-        Driver {
-            poll,
+        let handle = IoHandle {
+            registry,
             scheduled_io_map,
-        }
+        };
+        let driver = IoDriver { poll };
+
+        (driver, handle)
     }
 
-    pub fn add_source<E>(&mut self, source: &mut E)
-    where
-        E: Source,
-    {
-        let scheduled_io = ScheduledIo::default();
-        let token = scheduled_io.token();
-
-        self.scheduled_io_map.insert(token, scheduled_io);
-        self.poll.registry().register(source, token, DEFAULT_IO_INTERESTS);
-    }
-
-    pub fn park(&mut self) {
+    pub fn park(&mut self, handle: IoHandle) {
         let mut events = Events::with_capacity(MAX_EVENTS_CAPACITY);
         self.poll
             .poll(&mut events, None)
@@ -49,8 +47,21 @@ impl Driver {
         for event in &events {
             let token = event.token();
 
-            let io = self.scheduled_io_map.get(&token);
-            io.expect("No waker provided").wake();
+            let io = handle.scheduled_io_map.get(&token);
+            io.expect("No IO founded").wake();
         }
+    }
+}
+
+impl IoHandle {
+    pub fn add_source<E>(&mut self, source: &mut E)
+    where
+        E: Source,
+    {
+        let scheduled_io = ScheduledIo::default();
+        let token = scheduled_io.token();
+
+        self.scheduled_io_map.insert(token, scheduled_io);
+        let _ = self.registry.register(source, token, DEFAULT_IO_INTERESTS);
     }
 }
