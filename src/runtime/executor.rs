eabdc4c8dc3880b1224d17d135af935bf97ec786
Luis Teodoro <luislopesteo@hotmail.com>
2026-02-05 08:52:19 -0300

    feat: main loop considering IoDriver and task processing

--- src/runtime/executor.rs
+++ src/runtime/executor.rs
@@ -2,6 +2,7 @@ use futures::task::waker_ref;
 use std::{
     sync::{Arc, mpsc::Receiver},
     task::{Context, Poll},
+    time::Duration,
 };
 
 use crate::runtime::{
@@ -9,10 +10,17 @@ use crate::runtime::{
     task::Task,
 };
 
+const POLL_TIMEOUT: Duration = Duration::from_millis(100);
+
 /// Task executor that receives tasks off of a channel and runs them.
 pub struct ExecutorDriver {
     io_driver: IoDriver,
     ready_queue: Receiver<Arc<Task>>,
+    metadata: ExecutionData,
+}
+
+struct ExecutionData {
+    pending_tasks: i32,
 }
 
 pub struct ExecutorHandle {
@@ -29,9 +37,12 @@ impl ExecutorDriver {
     pub fn new(ready_queue: Receiver<Arc<Task>>) -> (ExecutorDriver, ExecutorHandle) {
         let (io_driver, io_handle) = IoDriver::new();
 
+        let metadata = ExecutionData { pending_tasks: 0 };
+
         let exec_driver = ExecutorDriver {
-            io_driver: io_driver,
+            io_driver,
             ready_queue,
+            metadata,
         };
 
         let exec_handle = ExecutorHandle {
@@ -41,10 +52,24 @@ impl ExecutorDriver {
         (exec_driver, exec_handle)
     }
 
-    pub fn start(&self) {
-        let mut pending_tasks = 0;
+    pub fn start(mut self, handle: &ExecutorHandle) {
+        loop {
+            // Try receive tasks from the channel
+            self.process_tasks();
 
-        while let Ok(task) = self.ready_queue.recv() {
+            let poll_timeout = if self.metadata.pending_tasks > 0 {
+                Duration::ZERO
+            } else {
+                POLL_TIMEOUT
+            };
+
+            // Park in IO driver waiting for IO events for a timeout
+            self.io_driver.park(&handle.io_handle, poll_timeout);
+        }
+    }
+
+    fn process_tasks(&mut self) {
+        while let Ok(task) = self.ready_queue.try_recv() {
             // Take the future, and if it has not yet completed (is still Some),
             // poll it in an attempt to complete it.
             let mut future_slot = task.future.lock().unwrap();
@@ -55,14 +80,14 @@ impl ExecutorDriver {
                 match future.as_mut().poll(context) {
                     Poll::Pending => {
                         *future_slot = Some(future);
-                        pending_tasks += 1;
+                        self.metadata.pending_tasks += 1;
                     }
                     Poll::Ready(_) => {
-                        pending_tasks -= 1;
+                        self.metadata.pending_tasks -= 1;
                     }
                 }
 
-                if pending_tasks == 0 {
+                if self.metadata.pending_tasks == 0 {
                     break;
                 }
             }
