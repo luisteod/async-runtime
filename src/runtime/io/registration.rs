0f7c7504f586c0157e66b43ab4c255b3ef24a424
Luis Teodoro <luislopesteo@hotmail.com>
2026-02-07 11:49:38 -0300

    fix: async_io method now returning the new future

--- src/runtime/io/registration.rs
+++ src/runtime/io/registration.rs
@@ -1,5 +1,7 @@
 use crate::runtime::{executor::ExecutorHandle, io::scheduled_io::ScheduledIo};
+use mio::Interest;
 use mio::event::Source;
+use std::io;
 use std::rc::Rc;
 
 pub struct Registration {
@@ -11,5 +13,24 @@ impl Registration {
         // Get the IoDriver via global context and register
         let io = exec_handle.io().add_source(source);
         Registration { io }
+    }
+
+    pub(crate) async fn async_io<R>(
+        &self,
+        interest: Interest,
+        mut f: impl FnMut() -> io::Result<R>,
+    ) -> io::Result<R> {
+        loop {
+            // This now returns an owned Future that we can await
+            let _event = ScheduledIo::io_future(&self.io, interest).await;
+
+            match f() {
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    // The OS said "try again later", so we loop and await again
+                    continue;
+                }
+                x => return x,
+            }
+        }
     }
 }
